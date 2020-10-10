b3b837d97239d4a8269f5c8bffef4b17583910b1
Kendell R <KTibow@users.noreply.github.com>
2020-10-10 07:23:27 -0700

    Update validate_code.py

--- validate_code.py
+++ validate_code.py
@@ -16,14 +16,9 @@ if len(glob.glob("**/*.py", recursive=True)) > 0:
     os.system("echo hey2")
     os.system(
         """docker run --name hassfest_instance --workdir /github/workspace --rm """
-        + """-v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" """
         + """-v "/home/runner/work/_temp/_runner_file_commands":"/github/file_commands" -v "/home/runner/work/blueprinttest/blueprinttest":"/github/workspace" hassfest"""
     )
-    #    """-e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_RETENTION_DAYS -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF """ +
-    #    """-e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_SERVER_URL -e GITHUB_API_URL -e GITHUB_GRAPHQL_URL -e GITHUB_WORKSPACE -e GITHUB_ACTION """ +
-    #    """-e GITHUB_EVENT_PATH -e GITHUB_PATH -e GITHUB_ENV -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL """ +
-    #    + """-e pythonLocation -e INPUT_GITHUB_TOKEN -e INPUT_CATEGORY -e HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY """
-    #    + """-e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL -e GITHUB_ACTIONS=true -e CI=true -v "/var/run/docker.sock":"/var/run/docker.sock" """
+    #    + """-v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" """
     endgroup()
     startgroup("Flake8: Unused stuff and docstrings")
     os.system("flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics")
