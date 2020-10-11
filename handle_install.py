1ad171aa2564e1fcc8d4bb6deddba3617ee87638
Kendell R <KTibow@users.noreply.github.com>
2020-10-10 17:31:40 -0700

    Update handle_install.py

--- handle_install.py
+++ handle_install.py
@@ -14,11 +14,11 @@ def endgroup():
 if len(glob.glob("**/*.py", recursive=True)) > 0:
     os.system("sh /home/runner/work/_actions/KTibow/ha-blueprint/main/python_setup.sh")
 else:
-    print("No python files found, not running hassfest and flake8.")
+    print("No python files found, not installing hassfest and flake8.")
 if len(glob.glob("**/*.js", recursive=True)) > 0:
     startgroup("ESLint setup")
     os.system("npm install")
     os.system("npm install eslint eslint-formatter-summary prettier")
     endgroup()
 else:
-    print("No JS files found, not running eslint.")
+    print("No JS files found, not installing eslint and prettier.")
