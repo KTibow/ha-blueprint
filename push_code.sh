e849d80489918618c589253f51bc3f51f4ab191d
Kendell R <KTibow@users.noreply.github.com>
2020-10-10 18:02:16 -0700

    Update push_code.sh

--- push_code.sh
+++ push_code.sh
@@ -3,11 +3,13 @@ then
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "GitHub Actions"
 git pull
-git status
 git add *.js
 git add *.ts
 git add *.py
 git status
+git log
 git commit --amend --no-edit || true
+git status
+git log
 git push --force | true
 fi
