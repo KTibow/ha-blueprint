ce4c5189f68614af78919287b3deaa8d6733424f
Kendell R <KTibow@users.noreply.github.com>
2020-10-10 17:49:20 -0700

    Update push_code.sh

--- push_code.sh
+++ push_code.sh
@@ -2,10 +2,12 @@ if [ $(git diff HEAD | wc -l) -gt 30 ]
 then
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "GitHub Actions"
+git pull
 git status
 git add *.js
 git add *.ts
 git add *.py
+git status
 git commit --amend --no-edit || true
 git push --force | true
 fi
