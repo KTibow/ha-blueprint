116edaa240e4e997a1b80e9985868d5049b1333d
Kendell R <KTibow@users.noreply.github.com>
2020-10-12 18:09:11 -0700

    Update format_code.py

--- format_code.py
+++ format_code.py
@@ -13,7 +13,7 @@ def endgroup():
 if os.getenv("FORMAT_CODE") != "DISABLED":
     if len(glob.glob("**/*.py", recursive=True)) > 0:
         startgroup("Python format")
-        os.system("sh /home/runner/work/_actions/KTibow/ha-blueprint/main/python_format.sh")
+        os.system("sh /home/runner/work/myaction/python_format.sh")
         endgroup()
     else:
         print("No python files found, not running isort and black.")
